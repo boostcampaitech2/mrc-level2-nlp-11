04da932820a4003a8f8f60b7248bc2c43c450371
root <mon823@naver.com>
2021-10-15 08:47:10 +0000

    [chore] fix path read/train.py

--- code/read/train.py
+++ code/read/train.py
@@ -21,7 +21,6 @@ from tokenizers.models import WordPiece
 
 from utils_qa import postprocess_qa_predictions, check_no_error
 from trainer_qa import QuestionAnsweringTrainer
-from retrieval import SparseRetrieval
 
 from arguments import (
     ModelArguments,
